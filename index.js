c6ec40c4815b7a003a2a4d78419bd7d993735b54
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-05-19 15:10:26 +0600

    create a POST API for storing new tutor data

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv');
 const app = express()
 const PORT = process.env.PORT || 8000
+dotenv.config();
+
+const uri = process.env.AUTH_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("MediQueue");
+        const tutorCollection = db.collection("tutorData");
+
+        // creating post api for add tutor data
+        app.post("/addTutor", async (req, res) => {
+            const tutors = req.body;
+            const result = await tutorCollection.insertOne(tutors);
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
